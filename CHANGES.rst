8c2e341e0864236cac409fbc837a9dc3bed4f774
Giacomo Monari <arsenico13@hotmail.it>
2018-03-15 14:34:27 +0100

    Preparing release 1.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,8 +2,8 @@ Changelog
 =========
 
 
-1.0a1 (unreleased)
-------------------
+1.0 (2018-03-15)
+----------------
 
 - Initial release. Support of Multipler service for the new wildcard.media's
   WildcardVideo content type.
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='wildcardmedia.multipler',
-    version='1.0a1',
+    version='1.0',
     description="Multipler adapter for a Wildcard.Video content type.",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
